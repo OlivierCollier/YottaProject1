6cdfeaa9504a6aa966d9d875cea1c0f60b011691
Jérémie Koster <jeremie.koster@gmail.com>
2021-02-17 09:51:53 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Product subscription project.',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is project 1 for Yotta bootcamp',
+    author='Jérémie Koster & Olivier Collier',
+    license='MIT',
+)
