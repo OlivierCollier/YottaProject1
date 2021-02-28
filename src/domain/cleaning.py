3dd067a1c45ca7851719b7d2737b98eef664391c
Olivier Collier <oliviercollier8@gmail.com>
2021-02-28 08:40:34 +0100

    :construction: Working on cleaning.py

---
+++ src/domain/cleaning.py
@@ -0,0 +1,37 @@
+
+import numpy as np
+import pandas as pd
+
+
+class ClientDataCleaner:
+
+	def __init__(self, data):
+		self.data = data
+
+	def _remove_
+
+
+
+
+
+class EcoDataCleaner:
+
+	"""
+	It is assumed that the data corresponds to consecutive months.
+	"""
+
+	def clean(self):
+
+	def _check_if_consecutive_months(self, date_format):
+
+	def _check_if_quarterly_data(self):
+
+	def _impute_quarterly_data(self):
+
+	def _impute_monthly_data(self):
+
+	def _detect_first_month_of_quarter(self):
+
+
+
+
