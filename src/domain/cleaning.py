d6ab4dfb73cafeb7960451abeca8108cf936380a
Olivier Collier <oliviercollier8@gmail.com>
2021-03-03 22:39:09 +0100

    :rocket: Finishing with cleaning.py

--- src/domain/cleaning.py
+++ src/domain/cleaning.py
@@ -1,104 +1,136 @@
 
-import numpy as np
 import pandas as pd
-import datetime as dt
+from sklearn.base import BaseEstimator, TransformerMixin
+from scipy.stats.mstats import winsorize
 
 
-class ClientDataCleaner:
+def impute_missing_eco_data(eco_data: pd.DataFrame) -> pd.DataFrame:
+	"""Impute missing values in economic data.
 
-	def __init__(self, data):
+	Parameters
+	----------
+	eco_data: pd.Dataframe
+
+	Returns
+	-------
+	complete_data: pd.Dataframe
+	"""
+	complete_data = eco_data.interpolate(limit_direction='both')
+	return complete_data
+
+
+def correct_wrong_entries(data: pd.DataFrame, corrections: dict) -> pd.DataFrame:
+	"""Correct erroneous entries in data.
+
+	Parameters
+	----------
+	data: pd.DataFrame
+	corrections: dict
+		A dictionary whose keys are the relevant columns to modify,
+		and whose values are dictionaries characterizing the changes to be made.
+
+	Returns
+	-------
+	corrected_data: pd.DataFrame
+	"""
+	corrected_data = data.replace(corrections)
+	return corrected_data
+
+
+class MissingValueTreatment(BaseEstimator, TransformerMixin):
+	"""Data transformations to deal with missing values.
+
+	When JOB_TYPE is provided, other related variables can be filled
+	by the most common value among observations with same JOB_TYPE value.
+	In other cases, the observation is removed.
+
+	Attributes
+	----------
+	data: pd.DataFrame
+	categorical_variables: list
+		List of categorical variables having missing values.
+	continuous_variables: list
+		List of continuous variables having missing values.
+
+	Methods
+	-------
+	fit(data)
+	transform(data)
+
+	"""
+
+	def __init__(self, categorical_variables: list, continuous_variables: list):
+		self.data = None
+		self.categorical_variables = categorical_variables
+		self.continuous_variables = continuous_variables
+
+	def fit(self, data: pd.DataFrame):
 		self.data = data
+		return self
 
-	def clean(self):
+	def transform(self, data: pd.DataFrame) -> pd.DataFrame:
+		"""Imputes missing values using JOB_TYPE then removes remaining ones."""
+		self._impute_from_job_type()
+		self._remove_remaining_rows_with_missing_values()
+		return self.data
 
+	def _impute_from_job_type(self):
+		"""Imputes missing values using JOB_TYPE."""
+		for column_name in self.categorical_variables:
+			method = (lambda x: x.mode()[0])
+			self._impute_single_column_from_job_type(column_name, method)
+		for column_name in self.continuous_variables:
+			method = (lambda x: x.median())
+			self._impute_single_column_from_job_type(column_name, method)
+		return self
 
-	def _remove_incoherent_values(self, column_name, value):
-		self.data[column_name].replace(to_replace=value, value=np.nan, inplace=True)
-
-	def _impute_from_job_type(self, column_name, method):
-		select_relevant_values = (~self.data['job_type'].isnull()) & (self.data[column_name].isnull())
-		to_replace = self.data.loc[select_relevant_values, column_name]
-		corresponding_job_types = self.data.loc[select_relevant_values, 'job_type']
-		replacements = self.data[['job_type', column_name]]\
-			.groupby('job_type')\
-			.agg(method)\
+	def _impute_single_column_from_job_type(self, column_name: str, method):
+		"""Imputes missing values for a single variable using JOB_TYPE."""
+		relevant_observations = (~self.data['JOB_TYPE'].isnull()) & (self.data[column_name].isnull())
+		corresponding_job_types = self.data.loc[relevant_observations, 'JOB_TYPE']
+		replacements = self.data[['JOB_TYPE', column_name]] \
+			.groupby('JOB_TYPE') \
+			.agg(method) \
 			.to_dict()
-		to_replace = corresponding_job_types.replace(replacements[column_name])
-
-	def _impute_categorical_from_job_type(self, column_name):
-		method = (lambda x: x.mode()[0])
-		self._impute_from_job_type(column_name, method)
-
-	def _impute_continuous_from_job_type(self):
-		method = (lambda x: x.median())
-		self._impute_from_job_type(column_name, method)
-
-	def _deal_with_outliers(self, column_name, method):
-		median = self.data[column_name].median()
-		variance = self.data[column_name].var()
-		outliers = abs(self.data[column_name]) > median + 2 * variance
-		if method == 'drop':
-			self.data = self.data[~outliers]
-		elif method == 'clip':
-			self.data[column_name].clip(lower=mean-2*variance, upper=mean+2*variance, inplace=True)
-		else:
-			raise ValueError(f'Method {method} not implemented')
+		self.data.loc[relevant_observations, column_name] = \
+			corresponding_job_types.replace(replacements[column_name])
+		return self
 
 	def _remove_remaining_rows_with_missing_values(self):
+		"""Removes remaining observations with missing values."""
 		self.data.dropna(inplace=True)
+		return self
 
-class EcoDataCleaner:
 
+class OutlierTreatment(BaseEstimator, TransformerMixin):
+	"""Data transformations to deal with outliers.
+
+	Attributes
+	----------
+	data: pd.DataFrame
+	column_names: list
+
+	Methods
+	-------
+	fit(data)
+	transform(data)
 	"""
-	Assumes that data has column "date" in format datetime.
 
-	"""
+	def __init__(self, column_names: list):
+		self.data = None
+		self.column_names = column_names
 
-	def __init__(self, data):#, quarterly_features):
+	def fit(self, data: pd.DataFrame):
 		self.data = data
-		#self.quarterly_features = quarterly_features
+		return self
 
-	#@property
-	#def monthly_features(self):
-		#		column_names = self.data.columns
-		#quarterly_features = self.data.quarterly_data
-		#monthly_features = column_names.difference(quarterly_features).to_list()
-		#return monthly_features
+	def transform(self, data: pd.DataFrame) -> pd.DataFrame:
+		"""Deal with outliers for given columns."""
+		self._winsorize()
+		return self.data
 
-	def clean(self):
-		self._check_if_consecutive_months()
-		self._impute_monthly_data()
-		#for column_name in self.quarterly_features:
-		#	self._impute_quarterly_data(column_name)
-
-	def _check_if_consecutive_months(self):
-		month_number = self.data['date'].apply(lambda x: 12 * x.year + x.month)
-		differences = month_number.diff()
-		test = all(differences.eq(1))
-		if not test:
-			raise ValueError('Months are not consecutive.')
-
-	def _impute_monthly_data(self):
-		self.data.interpolate(limit_direction='both', inplace=True)
-
-	#def _impute_quarterly_data(self, column_name):
-	#	length_of_first_quarter = self._detect_length_of_first_quarter(column_name)
-
-
-
-
-	#def _detect_length_of_first_quarter(self, column_name):
-	#	feature = self.data[column_name].reset_index(drop=True)
-	#	for length_first_quarter in range(3):
-	#		quarters = pd.Series(feature.index).apply(lambda x: (x-length_first_quarter) // 3)
-	#		numbers_of_values_per_quarters = feature.groupby(quarters).nunique()
-		#		test = all(numbers_of_values_per_quarters.isin([0, 1]))
-		#	if test:
-		#		return length_first_quarter
-		#raise ValueError(f'Feature {column_name} is not quarterly.')
-
-
-
-
-
-
+	def _winsorize(self):
+		"""Winsorizes outliers."""
+		for column_name in self.column_names:
+			self.data[column_name] = winsorize(self.data[column_name], limits=[0, .01])
+		return self
