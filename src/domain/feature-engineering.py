afd5ba485223370d0958a0c5614698bdaadbfcaa
Olivier Collier <oliviercollier8@gmail.com>
2021-03-03 09:42:42 +0100

    :construction: Working on feature-engineering.py

--- src/domain/feature-engineering.py
+++ src/domain/feature-engineering.py
@@ -3,8 +3,8 @@ import numpy as np
 import pandas as pd
 
 from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import LabelEncoder
-from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import LabelEncoder, FunctionTransformer, OneHotEncoder, Binarizer
+from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.base import BaseEstimator, TransformerMixin
 
 
@@ -20,33 +20,16 @@ class CategoricalToBinaryTransformer(BaseEstimator, TransformerMixin):
         transformed = X.eq(self.category).astype(int)
         return transformed
 
-
-class ToIndicatorTransformer(BaseEstimator, TransformerMixin):
-
-    def __init__(self, inequality_type, threshold):
-        self.inequality_type = inequality_type
-        self.threshold = threshold
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X, y=None):
-        if self.inequality_type == 'is_smaller':
-            transformed = X.le(threshold).astype(int)
-        elif self.inequality_type == 'is_larger':
-            transformed = X.ge(threshold).astype(int)
-        return transformed
+AgeTransformer = FeatureUnion([
+    ()
+])
 
 
-
-
-binary_features = ['HAS_DEFAULT', 'HAS_HOUSING_LOAN', 'HAS_PERSO_LOAN']
-
-
-
-processing_pipeline = ColumnTransformer(
-    [
-
-    ]
-
+pipeline = ColumnTransformer([
+    ('binary_transformer', OneHotEncoder(drop='first'), ['HAS_DEFAULT', 'HAS_HOUSING_LOAN', 'HAS_PERSO_LOAN']),
+    ('categorical_simplifier1', CategoricalToBinaryTransformer('Célibataire'), ['STATUS']),
+    ('categorical_simplifier2', CategoricalToBinaryTransformer('Succes'), ['RESULT_LAST_CAMPAIGN']),
+    ('indicator_transformer', Binarizer(), ['BALANCE', 'NB_DAY_LAST_CONTACT']),
+    ('inverse_transformer', FunctionTransformer(lambda x: 1/x), ['NB_DAY_LAST_CONTACT'])
+]
 )
