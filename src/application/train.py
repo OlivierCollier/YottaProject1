481211401204b3d28176d0016f88962d8414e28d
Jérémie Koster <jeremie.koster@gmail.com>
2021-03-04 22:28:14 +0100

    :sparkles: Build datasets in train.py

--- src/application/train.py
+++ src/application/train.py
@@ -3,23 +3,32 @@
     This class is used to train a ML model with some training date.
 """
 import argparse
-import ipdb
+import os
 
+import ipdb
+import src.config.base as base
+from src.infrastructure.build_dataset import DataBuilderFactory
 
 # Parse arguments
-parser = argparse.ArgumentParser(description='Files containing the training datasets')
+parser = argparse.ArgumentParser(description='Files containing the training datasets', \
+                                formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--client-data',
                 '-c', 
                 help='Provide the client dataset file',
-                dest='client_file',
-                required=True)
+                default=os.path.join(base.RAW_DATA_DIR, base.CLIENT_DATA_FILE_NAME),
+                dest='client_file')
 parser.add_argument('--eco-data',
                 '-e',
                 help='Provide the economic information dataset file',
-                dest='eco_file',
-                required=True)
+                default=os.path.join(base.RAW_DATA_DIR, base.ECO_DATA_FILE_NAME),
+                dest='eco_file')
 
 args = parser.parse_args()
 
 
+# Build datasets
+client_data = DataBuilderFactory(args.client_file, base.config_client_data, 'production', \
+                                base.CLIENT_COLUMNS_TO_DROP, base.ALL_CLIENT_DATA_TRANSLATION)
+eco_data = DataBuilderFactory(args.eco_file, base.config_eco_data, 'production', \
+                            base.ECO_COLUMNS_TO_DROP)
 
