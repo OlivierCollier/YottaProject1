5f679101136b72d7b0bdc09ccbca6bfb5af36d3d
Olivier Collier <oliviercollier8@gmail.com>
2021-03-08 23:42:54 +0100

    :monocle_face: Adding EDA notebook

--- notebooks/eda-notebook/eda_utilities.py
+++ notebooks/eda-notebook/eda_utilities.py
@@ -7,6 +7,7 @@ import warnings
 from IPython.core.display import display
 from scipy.stats import kstest, chi2_contingency
 from statsmodels.regression.linear_model import OLS
+import datetime as dt
 
 def settings():
     warnings.filterwarnings('ignore')
@@ -158,7 +159,155 @@ def show_mean_age_by_job_type(data):
     display(table)
 
 def replace_age123_by_mean_in_job_type(data):
-    replacements = data.loc[~data.AGE.eq(123), ['AGE', 'JOB_TYPE']].groupby('JOB_TYPE').agg('mean')
-    to_replace = data[data.AGE.eq(123)]
+    replacements = data.loc[~data.AGE.eq(123), ['AGE', 'JOB_TYPE']].groupby('JOB_TYPE').agg('mean').to_dict()
+    relevant_observations = data.AGE.eq(123)
+    corresponding_job_types = data.loc[relevant_observations, 'JOB_TYPE']
+    data.loc[relevant_observations, 'AGE'] = \
+        corresponding_job_types.replace(replacements['AGE'])
 
+def plot_age_histogram(data):
+    sns.displot(data,
+                x='AGE',
+                hue='SUBSCRIPTION',
+                stat='density',
+                common_norm=False,
+                element='step',
+                palette='bright')
+    plt.show()
 
+def plot_date_by_year(data):
+    weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    data['year-month'] = data['DATE_x'].apply(lambda x: x[:7])
+    data['date'] = data['DATE_x'].apply(lambda x: dt.datetime.strptime(x, '%Y-%m-%d'))
+    data['year'] = data['date'].apply(lambda x: x.year)
+    data['MONTH'] = data['date'].apply(lambda x: months[x.month-1])
+    data['WEEKDAY'] = data['date'].apply(lambda x: weekdays[x.weekday()])
+    sns.countplot(data.year)
+    plt.xlabel('YEAR')
+    plt.show()
+
+def plot_date_year_and_month(data):
+    sns.countplot(data['year-month'])
+    plt.xlabel('YEAR-MONTH')
+    plt.xticks([0, 7, 13, 19, 25], ('2008-05', '2009-01', '2009-07', '2010-01', '2010-07'))
+    plt.show()
+
+def show_table_subscription_by_weekday(data):
+    table = data[['WEEKDAY', 'SUBSCRIPTION']].groupby('WEEKDAY').agg('mean')['SUBSCRIPTION'].sort_values()
+    table = pd.DataFrame(table)
+    table.columns = ['Subscription rate']
+    display(table)
+
+def test_independence_between_target_and_weekday(data):
+    crosstab = pd.crosstab(data['SUBSCRIPTION'], data['year-month'])
+    test = chi2_contingency(crosstab)
+    print(f"La p-valeur du test du chi2 d'indépendance est de {test[1]} : le jour de la semaine n'est donc"
+          f" pas indépendant de la cible.")
+
+def show_table_subscription_by_month(data):
+    table = data[['MONTH', 'SUBSCRIPTION']].groupby('MONTH').agg('mean')['SUBSCRIPTION'].sort_values()
+    table = pd.DataFrame(table)
+    table.columns = ['Subscription rate']
+    display(table)
+
+def plot_education(data):
+    sns.countplot(data['EDUCATION'])
+    plt.show()
+
+def show_table_subscription_by_education(data):
+    table = data[['EDUCATION', 'SUBSCRIPTION']].groupby('EDUCATION').agg('mean')['SUBSCRIPTION'].sort_values()
+    table = pd.DataFrame(table)
+    table.columns = ['Subscription rate']
+    display(table)
+
+def plot_has_housing_and_perso_loan(data):
+    fig, ax = plt.subplots(1, 2, figsize=(12, 5))
+    sns.countplot(ax=ax[0], x='HAS_HOUSING_LOAN', data=data)
+    sns.countplot(ax=ax[1], x='HAS_PERSO_LOAN', data=data)
+    plt.show()
+
+def show_correlation_loan_variables_with_target(data):
+    data['HAS_HOUSING_LOAN'] = data['HAS_HOUSING_LOAN'].eq('Yes').astype(int)
+    data['HAS_PERSO_LOAN'] = data['HAS_PERSO_LOAN'].eq('Yes').astype(int)
+    data['HAS_LOAN'] = data['HAS_HOUSING_LOAN'] + data['HAS_PERSO_LOAN'] \
+                               - data['HAS_HOUSING_LOAN'] * data['HAS_PERSO_LOAN']
+    correlations = data[['HAS_PERSO_LOAN', 'HAS_HOUSING_LOAN', 'HAS_LOAN', 'SUBSCRIPTION']].corr()['SUBSCRIPTION']
+    correlations = pd.DataFrame(correlations)
+    correlations.drop(labels=['SUBSCRIPTION'], axis=0, inplace=True)
+    correlations.columns = ['Correlation with target']
+    display(correlations)
+
+def plot_has_default(data):
+    sns.countplot(data['HAS_DEFAULT'])
+    plt.show()
+
+def compute_correlation_has_default_target(data):
+    data['HAS_DEFAULT'] = data['HAS_DEFAULT'].eq('Yes').astype(int)
+    correlation = data[['HAS_DEFAULT', 'SUBSCRIPTION']].corr()['SUBSCRIPTION'][0]
+    print(f'La corrélation avec la cible est de {correlation}.')
+
+def missing_values_percentage_of_result_by_month(data):
+    table = data[['RESULT_LAST_CAMPAIGN', 'year-month']]\
+        .groupby('year-month')\
+        .agg(lambda x: 100 * x.isnull().sum() / len(x))
+    plt.plot(table)
+    plt.ylabel('Percentage')
+    plt.title('Percentage of missing values by month')
+    plt.xticks([0, 7, 13, 19, 25], ('2008-05', '2009-01', '2009-07', '2010-01', '2010-07'))
+    plt.show()
+
+def fill_missing_values_result_last_campaign(data):
+    data.loc[(data['NB_DAY_LAST_CONTACT'].ne(-1))&(data['RESULT_LAST_CAMPAIGN'].isnull()), 'RESULT_LAST_CAMPAIGN'] = "Other"
+    data.loc[data['RESULT_LAST_CAMPAIGN'].isnull(), 'RESULT_LAST_CAMPAIGN'] = "First contact"
+
+def show_table_subscription_by_result_last_campaign(data):
+    table = data[['RESULT_LAST_CAMPAIGN', 'SUBSCRIPTION']].groupby('RESULT_LAST_CAMPAIGN').agg('mean')
+    table = pd.DataFrame(table)
+    table.columns = ['Subscription rate']
+    display(table)
+
+def plot_nb_days_last_contact(data):
+    data['NB_DAYS_LAST_CONTACT'] = data['NB_DAY_LAST_CONTACT']
+    sns.histplot(data.loc[data.NB_DAYS_LAST_CONTACT.ne(-1), 'NB_DAYS_LAST_CONTACT'], kde=True)
+    plt.title('Distribution of NB_DAYS_LAST_CONTACT for nonnegative values')
+    plt.show()
+
+def compute_binned_nb_days_last_contact(data):
+    data_to_bin = data.loc[data['NB_DAY_LAST_CONTACT'].ne(-1), 'NB_DAY_LAST_CONTACT']
+    groups = pd.qcut(data_to_bin, 4)
+    data_to_bin = data_to_bin.groupby(groups).transform('mean')
+    data['BINNED_NB_DAYS_LAST_CONTACT'] = data['NB_DAY_LAST_CONTACT']
+    data.loc[data['NB_DAY_LAST_CONTACT'].ne(-1), 'BINNED_NB_DAYS_LAST_CONTACT'] = data_to_bin
+    data.loc[data['NB_DAY_LAST_CONTACT'].eq(-1), 'BINNED_NB_DAYS_LAST_CONTACT'] = data_to_bin.max()
+
+def compute_correlation_binned_nb_days_last_contact_target(data):
+    correlation = data[['SUBSCRIPTION', 'BINNED_NB_DAYS_LAST_CONTACT']].corr()['SUBSCRIPTION'][1]
+    print(f'La corrélation entre le nouveau feature et la cible est de {correlation}.')
+
+def histogram_nb_contacts(data):
+    plt.hist(data['NB_CONTACT'])
+    plt.ylabel('Count')
+    plt.xlabel('NB_CONTACTS')
+    plt.title('Distribution of NB_CONTACTS')
+    plt.show()
+
+def clip_nb_contacts(data):
+    data['CLIPPED_NB_CONTACT'] = np.clip(data['NB_CONTACT'], a_min=0, a_max=15)
+
+def histogram_clipped_nb_contacts(data):
+    plt.hist(data['CLIPPED_NB_CONTACT'])
+    plt.ylabel('Count')
+    plt.xlabel('clipped NB_CONTACTS')
+    plt.title('Distribution of clipped NB_CONTACTS')
+    plt.show()
+
+def clip_nb_contacts_last_campaign(data):
+    data['CLIPPED_NB_CONTACT_LAST_CAMPAIGN'] = np.clip(data['NB_CONTACT_LAST_CAMPAIGN'], a_min=0, a_max=15)
+
+def histogram_clipped_nb_contacts_last_campaign(data):
+    plt.hist(data['CLIPPED_NB_CONTACT_LAST_CAMPAIGN'])
+    plt.ylabel('Count')
+    plt.xlabel('clipped NB_CONTACTS_LAST_CAMPAIGN')
+    plt.title('Distribution of clipped NB_CONTACTS_LAST_CAMPAIGN')
+    plt.show()
